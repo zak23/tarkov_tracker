e512dcfaef499bedd9730e7f8927bdae4a09135b
Zak23 <zak@zakozbourne.com>
2024-05-10 12:06:26 +1000

    Adding auto WS IP

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 // app.js
 
 // WebSocket connection
-const ws = new WebSocket('ws://192.168.1.144:3034');
+// const ws = new WebSocket('ws://192.168.1.144:3034');
+const ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
 
 // Function to fetch updated player information
 function updateLeaderboard() {
