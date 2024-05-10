4eea2059911e8d3c8bb6037414475ebf2e2d8a74
Zak23 <zak@zakozbourne.com>
2024-05-10 14:40:25 +1000

    Updating auto script

--- update.sh
+++ update.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Define the custom container name
+custom_container_name="tarkovtracker"
+
+# Check if a container with the specified name is running
+if [ "$(docker ps -q -f name=$custom_container_name)" ]; then
+    # Stop the container if it's running
+    echo "Stopping existing container with name $custom_container_name..."
+    docker stop $custom_container_name
+fi
+
 # Pull changes from Git
 git pull
 
 # Build Docker image with no cache
 docker build --no-cache -t tarkov_tracker .
 
-# Run Docker container
-docker run -v /home/docker/fika/user/profiles:/usr/src/app/profiles -p 3034:3034 tarkov_tracker --name tarkov_tracker
+# Run Docker container with custom name
+docker run --name $custom_container_name -v /home/docker/fika/user/profiles:/usr/src/app/profiles -p 3034:3034 tarkov_tracker
